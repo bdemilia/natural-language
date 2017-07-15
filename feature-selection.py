ed0a6ff06fb37f169a56f6a04ffa26f3899f71f6
bdemilia <benjamin.demilia@gmail.com>
2017-07-14 19:07:23 -0500

    Update feature-selection.py

--- feature-selection.py
+++ feature-selection.py
@@ -25,3 +25,232 @@ dfqa.shape
 from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer
 mq1 = TfidfVectorizer(max_features = 256).fit_transform(dfqa['question'].values)
 mq1
+
+diff_encodings = np.abs(mq1[::2] - mq1[1::2])
+diff_encodings
+
+from sklearn.manifold import TSNE
+tsne = TSNE(
+    n_components=3,
+    init='random', # pca
+    random_state=101,
+    method='barnes_hut',
+    n_iter=200,
+    verbose=2,
+    angle=0.5
+).fit_transform(diff_encodings.toarray())
+
+trace1 = go.Scatter3d(
+    x=tsne[:,0],
+    y=tsne[:,1],
+    z=tsne[:,2],
+    mode='markers',
+    marker=dict(
+        sizemode='diameter',
+        color = dfs['is_duplicate'].values,
+        colorscale = 'Portland',
+        colorbar = dict(title = 'duplicate'),
+        line=dict(color='rgb(255, 255, 255)'),
+        opacity=0.75
+    )
+)
+
+data=[trace1]
+layout=dict(height=800, width=800, title='test')
+fig=dict(data=data, layout=layout)
+py.iplot(fig, filename='3DBubble')
+
+df['q1len'] = df['question1'].str.len()
+df['q2len'] = df['question2'].str.len()
+
+df['q1_n_words'] = df['question1'].apply(lambda row: len(row.split(" ")))
+df['q2_n_words'] = df['question2'].apply(lambda row: len(row.split(" ")))
+
+def normalized_word_share(row):
+    w1 = set(map(lambda word: word.lower().strip(), row['question1'].split(" ")))
+    w2 = set(map(lambda word: word.lower().strip(), row['question2'].split(" ")))    
+    return 1.0 * len(w1 & w2)/(len(w1) + len(w2))
+
+
+df['word_share'] = df.apply(normalized_word_share, axis=1)
+
+df.head()
+
+plt.figure(figsize=(12, 8))
+plt.subplot(1,2,1)
+sns.violinplot(x = 'is_duplicate', y = 'word_share', data = df[0:50000])
+plt.subplot(1,2,2)
+sns.distplot(df[df['is_duplicate'] == 1.0]['word_share'][0:10000], color = 'green')
+sns.distplot(df[df['is_duplicate'] == 0.0]['word_share'][0:10000], color = 'red')
+
+df_subsampled = df[0:2000]
+
+trace = go.Scatter(
+    y = df_subsampled['q2len'].values,
+    x = df_subsampled['q1len'].values,
+    mode='markers',
+    marker=dict(
+        size= df_subsampled['word_share'].values * 60,
+        color = df_subsampled['is_duplicate'].values,
+        colorscale='Portland',
+        showscale=True,
+        opacity=0.5,
+        colorbar = dict(title = 'duplicate')
+    ),
+    text = np.round(df_subsampled['word_share'].values, decimals=2)
+)
+data = [trace]
+
+layout= go.Layout(
+    autosize= True,
+    title= 'Scatter plot of character lengths of question one and two',
+    hovermode= 'closest',
+        xaxis=dict(
+        showgrid=False,
+        zeroline=False,
+        showline=False
+    ),
+    yaxis=dict(
+        title= 'Question 2 length',
+        ticklen= 5,
+        gridwidth= 2,
+        showgrid=False,
+        zeroline=False,
+        showline=False,
+    ),
+    showlegend= False
+)
+fig = go.Figure(data=data, layout=layout)
+py.iplot(fig,filename='scatterWords')
+
+from IPython.display import display, HTML
+
+df_subsampled['q_n_words_avg'] = np.round((df_subsampled['q1_n_words'] + df_subsampled['q2_n_words'])/2.0).astype(int)
+print(df_subsampled['q_n_words_avg'].max())
+df_subsampled = df_subsampled[df_subsampled['q_n_words_avg'] < 20]
+df_subsampled.head()
+
+word_lens = sorted(list(df_subsampled['q_n_words_avg'].unique()))
+# make figure
+figure = {
+    'data': [],
+    'layout': {
+        'title': 'Scatter plot of char lenghts of Q1 and Q2 (size ~ word share similarity)',
+    },
+    'frames': []#,
+    #'config': {'scrollzoom': True}
+}
+
+# fill in most of layout
+figure['layout']['xaxis'] = {'range': [0, 200], 'title': 'Q1 length'}
+figure['layout']['yaxis'] = {
+    'range': [0, 200],
+    'title': 'Q2 length'#,
+    #'type': 'log'
+}
+figure['layout']['hovermode'] = 'closest'
+
+figure['layout']['updatemenus'] = [
+    {
+        'buttons': [
+            {
+                'args': [None, {'frame': {'duration': 300, 'redraw': False},
+                         'fromcurrent': True, 'transition': {'duration': 300, 'easing': 'quadratic-in-out'}}],
+                'label': 'Play',
+                'method': 'animate'
+            },
+            {
+                'args': [[None], {'frame': {'duration': 0, 'redraw': False}, 'mode': 'immediate',
+                'transition': {'duration': 0}}],
+                'label': 'Pause',
+                'method': 'animate'
+            }
+        ],
+        'direction': 'left',
+        'pad': {'r': 10, 't': 87},
+        'showactive': False,
+        'type': 'buttons',
+        'x': 0.1,
+        'xanchor': 'right',
+        'y': 0,
+        'yanchor': 'top'
+    }
+]
+
+sliders_dict = {
+    'active': 0,
+    'yanchor': 'top',
+    'xanchor': 'left',
+    'currentvalue': {
+        'font': {'size': 20},
+        'prefix': 'Avg. number of words in both questions:',
+        'visible': True,
+        'xanchor': 'right'
+    },
+    'transition': {'duration': 300, 'easing': 'cubic-in-out'},
+    'pad': {'b': 10, 't': 50},
+    'len': 0.9,
+    'x': 0.1,
+    'y': 0,
+    'steps': []
+}
+
+# make data
+word_len = word_lens[0]
+dff = df_subsampled[df_subsampled['q_n_words_avg'] == word_len]
+data_dict = {
+    'x': list(dff['q1len']),
+    'y': list(dff['q2len']),
+    'mode': 'markers',
+    'text': list(dff['is_duplicate']),
+    'marker': {
+        'sizemode': 'area',
+        #'sizeref': 200000,
+        'colorscale': 'Portland',
+        'size': dff['word_share'].values * 120,
+        'color': dff['is_duplicate'].values,
+        'colorbar': dict(title = 'duplicate')
+    },
+    'name': 'some name'
+}
+figure['data'].append(data_dict)
+
+# make frames
+for word_len in word_lens:
+    frame = {'data': [], 'name': str(word_len)}
+    dff = df_subsampled[df_subsampled['q_n_words_avg'] == word_len]
+
+    data_dict = {
+        'x': list(dff['q1len']),
+        'y': list(dff['q2len']),
+        'mode': 'markers',
+        'text': list(dff['is_duplicate']),
+        'marker': {
+            'sizemode': 'area',
+            #'sizeref': 200000,
+            'size': dff['word_share'].values * 120,
+            'colorscale': 'Portland',
+            'color': dff['is_duplicate'].values,
+            'colorbar': dict(title = 'duplicate')
+        },
+        'name': 'some name'
+    }
+    frame['data'].append(data_dict)
+
+    figure['frames'].append(frame)
+    slider_step = {'args': [
+        [word_len],
+        {
+            'frame': {'duration': 300, 'redraw': False},
+            'mode': 'immediate',
+            'transition': {'duration': 300}
+        }
+     ],
+     'label': word_len,
+     'method': 'animate'}
+    sliders_dict['steps'].append(slider_step)
+
+    
+figure['layout']['sliders'] = [sliders_dict]
+
+py.iplot(figure)
